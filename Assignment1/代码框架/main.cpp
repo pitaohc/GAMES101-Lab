a93cd00ff354383ec8c2e48055cc9e81d9cb16ec
pitaohc_games101 <pitaohc@qq.com>
2022-10-08 11:23:00 +0800

    完成Assignment1的基础部分

--- Assignment1/代码框架/main.cpp
+++ Assignment1/代码框架/main.cpp
@@ -6,74 +6,111 @@
 
 constexpr double MY_PI = 3.1415926;
 
-Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
-{
+
+const float deg2rad(const float &deg);
+
+const Eigen::Matrix4f rotate(float angle);
+
+const Eigen::Matrix4f translate(const Eigen::Vector3f &orientation);
+
+const Eigen::Matrix4f scale(const Eigen::Vector3f &percentage);
+
+/**
+ * 得到视角变换矩阵
+ * @param eye_pos 摄像机位置向量
+ * @return 视角变换矩阵
+ * */
+Eigen::Matrix4f get_view_matrix(const Eigen::Vector3f& eye_pos) {
     Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
 
-    Eigen::Matrix4f translate;
-    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
-        -eye_pos[2], 0, 0, 0, 1;
-
-    view = translate * view;
+    view = translate(-eye_pos) * view;
 
     return view;
 }
 
-Eigen::Matrix4f get_model_matrix(float rotation_angle)
-{
+/**
+ * 获得模型变换矩阵
+ * 当前任务中，只会进行旋转
+ * @param rotation_angle 旋转角度
+ * @return 模型变换矩阵
+ * */
+Eigen::Matrix4f get_model_matrix(float rotation_angle) {
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
 
-    // TODO: Implement this function
     // Create the model matrix for rotating the triangle around the Z axis.
     // Then return it.
-
+    model = rotate(rotation_angle);
     return model;
 }
 
+
+/**
+ * 获得投影变换矩阵
+ * @param eye_fov 视角fov
+ * @param aspect_ratio 视野宽高比
+ * @param zNear 视锥近平面
+ * @param zFar 视锥远平面
+ * @return 投影变换矩阵
+ * */
 Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
-                                      float zNear, float zFar)
-{
+                                      float zNear, float zFar) {
     // Students will implement this function
 
     Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
-
-    // TODO: Implement this function
     // Create the projection matrix for the given parameters.
     // Then return it.
+    //1. 计算宽高
+    float height = 2 * zNear * tan(deg2rad(eye_fov / 2)), width = height * aspect_ratio;
 
-    return projection;
+    //2. 透视投影
+    projection << zNear, 0, 0, 0,
+            0, zNear, 0, 0,
+            0, 0, zNear + zFar, -zNear * zFar,
+            0, 0, 1, 0;
+
+    //3. 正交投影
+    Eigen::Matrix4f my_scale = scale({-1 / width, -1 / height, 1 / (zFar - zNear)});
+    Eigen::Matrix4f my_translate = translate({0, 0, zNear + (zFar - zNear) / 2});
+
+    return my_scale * my_translate * projection;
 }
 
-int main(int argc, const char** argv)
-{
-    float angle = 0;
-    bool command_line = false;
-    std::string filename = "output.png";
+int main(int argc, const char **argv) {
+    float angle = 0; //旋转角度
+    bool command_line = false; //是否有命令行
+    std::string filename = "output.png"; //保存图片名称
 
+    //read argument ./rasterizer -r {angle} {filename}
     if (argc >= 3) {
         command_line = true;
         angle = std::stof(argv[2]); // -r by default
         if (argc == 4) {
             filename = std::string(argv[3]);
         }
-        else
-            return 0;
     }
+    std::cout << "Argument angle: " << angle << std::endl;
+    std::cout << "Argument filename: " << filename << std::endl;
 
+    //Set the view width and height
     rst::rasterizer r(700, 700);
 
+    //Set the viewport position
     Eigen::Vector3f eye_pos = {0, 0, 5};
 
-    std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
-
+    //Set the triangle position
+    std::vector<Eigen::Vector3f> pos{{2,  0, -2},
+                                     {0,  2, -2},
+                                     {-2, 0, -2}};
+    //Bing the point with index
     std::vector<Eigen::Vector3i> ind{{0, 1, 2}};
 
-    auto pos_id = r.load_positions(pos);
-    auto ind_id = r.load_indices(ind);
+    //load triangle
+    auto pos_id = r.load_positions(pos); //handle of position
+    auto ind_id = r.load_indices(ind); //handle of index
 
-    int key = 0;
-    int frame_count = 0;
-
+    int key = 0; //接受键盘输入
+    int frame_count = 0;// statistics frame count
+    /* if we input command with arguments, the program will generate a picture then return. */
     if (command_line) {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
@@ -89,16 +126,22 @@ int main(int argc, const char** argv)
 
         return 0;
     }
-
+    //debug
+//    angle = 70;
+    //end debug
     while (key != 27) {
-        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth); //清除帧缓冲区和深度缓冲区
+        std::cout << "angle:\n" << angle << std::endl;
+//        std::cout << "model transform:\n" << get_model_matrix(angle) << std::endl;
+        //设置MVP变换矩阵
+        r.set_model(get_model_matrix(angle)); //model transform
+        r.set_view(get_view_matrix(eye_pos));   //view transform
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));//projection transform
 
-        r.set_model(get_model_matrix(angle));
-        r.set_view(get_view_matrix(eye_pos));
-        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
-
+        //draw triangle to frame buffer
         r.draw(pos_id, ind_id, rst::Primitive::Triangle);
 
+        //read frame buffer
         cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
         image.convertTo(image, CV_8UC3, 1.0f);
         cv::imshow("image", image);
@@ -108,11 +151,56 @@ int main(int argc, const char** argv)
 
         if (key == 'a') {
             angle += 10;
-        }
-        else if (key == 'd') {
+        } else if (key == 'd') {
             angle -= 10;
         }
     }
 
     return 0;
+}
+
+/**
+ * 角度转换成弧度
+ * */
+const float deg2rad(const float &deg) {
+    return (float) (deg / 180.0 * acos(-1));
+}
+
+/**
+ * 旋转指定角度
+ * */
+const Eigen::Matrix4f rotate(float angle) {
+    Eigen::Matrix4f matrix;
+    float cosx = cos(deg2rad(angle));
+    float sinx = sin(deg2rad(angle));
+    matrix << cosx, -sinx, 0, 0,
+            sinx, cosx, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1;
+    return matrix;
+}
+
+/**
+ * 缩放三个轴到指定大小
+ * */
+const Eigen::Matrix4f scale(const Vector3f &percentage) {
+    Eigen::Matrix4f result;
+    result << percentage[0], 0, 0, 0,
+            0, percentage[1], 0, 0,
+            0, 0, percentage[2], 0,
+            0, 0, 0, 1;
+    return result;
+}
+
+/**
+ * 平移对象
+ * @param orientation 方向向量
+ * */
+const Eigen::Matrix4f translate(const Vector3f &orientation) {
+    Eigen::Matrix4f result;
+    result << 1, 0, 0, orientation[0],
+            0, 1, 0, orientation[1],
+            0, 0, 1, orientation[2],
+            0, 0, 0, 1;
+    return result;
 }
